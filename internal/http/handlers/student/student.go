6141305c05a4b4027f312337dd5c02e298a131f5
maaviah17 <muawiyah1705@gmail.com>
2025-10-29 11:36:41 +0530

    added request validation

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/maaviah17/students-api/internal/types"
 	"github.com/maaviah17/students-api/internal/utils/response"
+	"github.com/go-playground/validator/v10"
 )
 
 func New() http.HandlerFunc {
@@ -21,12 +22,20 @@ func New() http.HandlerFunc {
 		//whatever data that is coming in will be json decoded and stored in the struct.
 		err := json.NewDecoder(r.Body).Decode(&newStudent)
 		if errors.Is(err, io.EOF) {
-			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(fmt.Errorf("empty bodyy aaayi hai :(( ))")))
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(fmt.Errorf("empty bodyy aaayi hai :(( ")))
 			return 
 		}
 
 		if err != nil {
 			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return 
+		}
+
+		//request validation
+		if err := validator.New().Struct(newStudent); err != nil {
+			validateErrs := err.(validator.ValidationErrors)
+			response.WriteJson(w, http.StatusBadRequest, response.ValidationError(validateErrs))
+			return 
 		}
 
 		response.WriteJson(w, http.StatusCreated, map[string]string {"success":"OK"})
